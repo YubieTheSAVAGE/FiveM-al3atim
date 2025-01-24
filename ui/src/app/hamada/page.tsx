0240c1ce2748fb47b6dce48c6086e5f6f62785fd
YubieTheSAVAGE <ayoub04bd@gmail.com>
2025-01-24 23:30:04 +0100

    initialize Next.js project with Redux, Tailwind CSS, and FiveM integration

---
+++ ui/src/app/hamada/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Card from "@/components/Card";
+
+export default function Hamada() {
+    return (
+        <div className="m-20">
+            <h1>Hamada</h1>
+            <Card />
+        </div>
+    );
+}
---
+++ ui/src/components/Card.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+
+
+export default function Card() {
+    return (
+        <div className="w-[220px] h-[280px] space-y-2 rounded-lg shadow-2xl p-2 border-2 border-gray-800">
+            <div className="h-3/5 flex items-center justify-center relative">
+                <Image
+                    src="/coce.png"
+                    width={100}
+                    height={100}
+                    alt="coca cola"
+                />
+                <Image
+                    src="/piece.png"
+                    width={30}
+                    height={30}
+                    alt="piece"
+                    className="bottom-0 right-0 absolute"
+                />
+            </div>
+            <div className="h-0.5 w-9/12 m-auto bg-gradient-to-r from-white via-red-700 to-white"></div>
+            <div className="flex justify-between items-center">
+                <div>
+                    <h1 className="text-sm font-extrabold">كاس كوكا باريد</h1>
+                    <p className="text-xs font-medium">المخزون : 10</p>
+                    <p className="text-xs font-medium">الخبرة : 10</p>
+                </div>
+                <div className="text-right font-bold text-white flex flex-col items-end">
+                    <div className="text-xs bg-[#95BF3B] py-0.5 rounded-tr-xl rounded-bl-xl w-min relative">
+                        <span className="border border-[#F21D2F] absolute h-7 rotate-[60deg] right-1/2 -top-1"></span>
+                        120.<span className="text-[8px]">00</span>
+                    </div>
+                    <div className="bg-[#F21D2F] rounded-tl-xl rounded-br-xl text-xl leading-none py-0.5 px-1">
+                        <p className="text-[6px]">SR ريال</p>
+                        99.00
+                    </div>
+                </div>
+            </div>
+            <button className="w-full rounded-full bg-[#95BF3B] font-bold text-xs p-1 hover:bg-[#8ab137] duration-75">شراء</button>
+        </div>
+    );
+}
